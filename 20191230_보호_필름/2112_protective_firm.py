968355196bb00c56a3c552934ee98f4a6947c758
HanSeokhyeon <sah0322@naver.com>
2019-12-30 16:45:14 +0900

    fail

--- 20191230_보호_필름/2112_protective_firm.py
+++ 20191230_보호_필름/2112_protective_firm.py
@@ -3,3 +3,66 @@
 """
 
 """
+
+
+import sys
+from collections import deque
+
+'''
+      아래의 구문은 input.txt 를 read only 형식으로 연 후,
+      앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+      여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+      아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+
+      따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
+      아래 구문을 사용하기 위해서는 import sys가 필요합니다.
+
+      단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
+'''
+sys.stdin = open("sample_input.txt", "r")
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+
+
+def evaluate_performance():
+    w_cnt = 0
+    for w in range(width):
+        same_cnt = 0
+        for d in range(depth):
+            if d != 0:
+                if before == firm[d][w]:
+                    same_cnt += 1
+                else:
+                    same_cnt == 0
+            before = firm[d][w]
+            if same_cnt == k:
+                w_cnt += 1
+
+    return w_cnt == width
+
+
+def bfs():
+    q = deque()
+    for a in range(depth):
+        q.append((a, [a]))
+
+    while q:
+        node, path = q.popleft()
+
+
+
+for test_case in range(1, T + 1):
+    depth, width, k = map(int, input().split())
+    firm = []
+    for i in range(depth):
+        firm.append(list(map(int, input().split())))
+    if evaluate_performance():
+        print(0)
+        continue
+    bfs()
+
+
+
+
+
